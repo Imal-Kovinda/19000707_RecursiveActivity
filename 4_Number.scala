bb82a0f0595eeb1032b469bd12856a195d390e73
Imal-Kovinda <83854345+Imal-Kovinda@users.noreply.github.com>
2021-08-02 12:17:29 +0530

    Update 4_Number.scala

--- 4_Number.scala
+++ 4_Number.scala
@@ -2,11 +2,15 @@ package act
 
 object Number {
   def num(n:Int){
-    if(n%2==0){
-      println(n + " is an Even number");
-    }
-    else{
-      println(n + " is an Odd number");
+    if(n>0){
+      if(n%2==0){
+        println(n + " is an Even number");
+        num(n-1);
+      }
+      else{
+        println(n + " is an Odd number");
+        num(n-1);
+      } 
     }
   }
   
